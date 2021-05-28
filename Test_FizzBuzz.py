0e4e88b9a98ea041c00faa6a0ba48fea325da8bb
Nicole <nyarbrough14@gmail.com>
2021-05-28 11:05:02 -0700

    Wrote first test where num=1

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -2,6 +2,9 @@ import unittest
 import FizzBuzz
 
 class testCaseAdd(unittest.TestCase):
+    def first_test(self):
+        #self.assertEqual(FizzBuzz.FizzBuzz(0), "")
+        self.assertEqual(FizzBuzz.FizzBuzz(1), "1")
 
 if __name__ == '__main__':
     unittest.main()
