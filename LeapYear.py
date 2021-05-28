3b565d95579a739e9f6ac8f203fd11fe31aa75dd
Nicole <nyarbrough14@gmail.com>
2021-05-28 14:54:07 -0700

    Added code to pass test year=400

--- LeapYear.py
+++ LeapYear.py
@@ -1,6 +1,8 @@
 def LeapYear( year ):
     if (year % 4 == 0):
         if (year % 100 == 0):
+            if (year % 400 == 0):
+                return True
             return False
         return True
     return False
