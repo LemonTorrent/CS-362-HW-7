40dc117f9912f801f56e4820199f312714cee636
Nicole <nyarbrough14@gmail.com>
2021-05-28 11:14:45 -0700

    Added new test where num=0

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -2,10 +2,11 @@ import unittest
 import FizzBuzz
 
 class testCaseAdd(unittest.TestCase):
-    def test_test(self):
+    def test1(self):
         self.assertEqual(FizzBuzz.FizzBuzz(1), "1 ")
 
-        #self.assertEqual(FizzBuzz.FizzBuzz(0), "")
+    def test0(self):
+        self.assertEqual(FizzBuzz.FizzBuzz(0), "")
 
 if __name__ == '__main__':
     unittest.main()
