849e5c37b8a9a09eaaa109e63e7bec041fcfb3cd
Nicole <nyarbrough14@gmail.com>
2021-05-28 12:06:52 -0700

    Added test num=3

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -8,5 +8,8 @@ class testCaseAdd(unittest.TestCase):
     def test0(self):
         self.assertEqual(FizzBuzz.FizzBuzz(0), "")
 
+    def test3(self):
+        self.assertEqual(FizzBuzz.FizzBuzz(3), "1 2 Fizz ")
+
 if __name__ == '__main__':
     unittest.main()
