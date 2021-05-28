50d88449e7f1c95e23461dccdc84aa40efe98ca8
Nicole <nyarbrough14@gmail.com>
2021-05-28 12:49:57 -0700

    Wrote test year=100

--- Test_LeapYear.py
+++ Test_LeapYear.py
@@ -5,5 +5,8 @@ class testCaseAdd(unittest.TestCase):
     def test4(self):
         self.assertEqual(LeapYear.LeapYear(4), True)
 
+    def test100(self):
+        self.assertEqual(LeapYear.LeapYear(100), False)
+
 if __name__ == '__main__':
     unittest.main()
