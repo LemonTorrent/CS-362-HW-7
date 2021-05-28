7664c04ee255a96c17d922853694d508d7e8a1a7
Nicole <nyarbrough14@gmail.com>
2021-05-28 14:55:51 -0700

    Added new tests where year=0,2000,2100 that the program already passes.

--- Test_LeapYear.py
+++ Test_LeapYear.py
@@ -11,5 +11,14 @@ class testCaseAdd(unittest.TestCase):
     def test400(self):
         self.assertEqual(LeapYear.LeapYear(400), True)
 
+    def test0(self):
+        self.assertEqual(LeapYear.LeapYear(0), True)
+
+    def test2000(self):
+        self.assertEqual(LeapYear.LeapYear(2000), True)
+
+    def test2100(self):
+        self.assertEqual(LeapYear.LeapYear(2100), False)
+
 if __name__ == '__main__':
     unittest.main()
