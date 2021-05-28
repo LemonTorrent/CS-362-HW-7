ee834360869f0277054c5fd3a8810dca2f74354d
Nicole <nyarbrough14@gmail.com>
2021-05-28 12:19:01 -0700

    Wrote code so FizzBuzz can pass num=15

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,15 +3,18 @@ def FizzBuzz( num ):
     #print (num)
     print()
     for i in range(num):
+        intStatement = ""
         if ((i + 1) % 3 == 0):
-            print("Fizz")
-            retStatement += "Fizz"
-        elif ((i + 1) % 5 == 0):
-            print("Buzz")
-            retStatement += "Buzz"
-        else:
-            print(i+1)
-            retStatement += str(i+1)
+            #print("Fizz")
+            intStatement += "Fizz"
+        if ((i + 1) % 5 == 0):
+            #print("Buzz")
+            intStatement += "Buzz"
+        if (((i + 1) % 3 != 0) and ((i + 1) % 5 != 0)):
+            #print(i+1)
+            intStatement += str(i+1)
+        print(intStatement)
+        retStatement += intStatement
         retStatement += " "
 
     return retStatement
